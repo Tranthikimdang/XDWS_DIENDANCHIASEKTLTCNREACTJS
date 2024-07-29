d4c0065121b6a8e8a83626a6f6099d5c52bb4fc8
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-07-29 15:14:50 +0700

    Update formComment.js

--- src/layouts/comment/data/formComment.js
+++ src/layouts/comment/data/formComment.js
@@ -12,30 +12,34 @@ function FormAddCmt() {
     console.log(data);
   };
 
+  const smallFontStyle = { 
+    fontSize: '0.9rem'
+  };
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <div className='container'>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <label className='text-light form-label'>Author</label>
+            <label className='text-light form-label' style={smallFontStyle}>Author</label>
             <input className='form-control bg-dark text-light' {...register('author', { required: true, minLength: 3, maxLength: 20 })} />
             {errors.author && <span className='text-danger'>{errors.author.type === 'required' ? 'Author is required' : errors.author.type === 'minLength' ? 'Name must be at least 3 characters long' : 'Name must be less than 20 characters long'}</span>}
           </div>
           <div>
-            <label className='text-light form-label'>Function</label>
+            <label className='text-light form-label' style={smallFontStyle}>Function</label>
             <input className='form-control bg-dark text-light' {...register('function', { required: true, pattern: /^\S+@\S+$/i })} />
             {errors.function && <span className='text-danger'>{errors.function.type === 'required' ? 'Functionil is required' : 'Invalid Function address'}</span>}
           </div>         
           <div>
-            <label className='text-light form-label'>Description</label>
+            <label className='text-light form-label' style={smallFontStyle}>Description</label>
             <textarea className='form-control bg-dark text-light' {...register('description', { required: true, minLength: 10, maxLength: 100 })} />
             {errors.description && <span className='text-danger'>{errors.description.type === 'required' ? 'Description is required' : errors.description.type === 'minLength' ? 'Description must be at least 10 characters long' : 'Description must be less than 100 characters long'}</span>}
           </div>                  
           <div className='mt-3'>
             <button className='text-light btn btn-outline-info' type="submit">Add</button>
             <Link to="/comment">
-              <button className='text-light btn btn-outline-warning ms-2' type="button">Cancel</button>
+              <button className='text-light btn btn-outline-secondary ms-2' type="button">Back</button>
             </Link>
           </div>
         </form>
