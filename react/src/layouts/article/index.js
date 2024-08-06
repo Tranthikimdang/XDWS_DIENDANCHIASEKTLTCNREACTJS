c718cb72cc53c1e4fece1ce15ee086b56d0ff628
Tranthikimdang <dangttkpc05468@fpt.edu.vn>
2024-08-06 15:22:19 +0700

    fix

--- react/src/layouts/article/index.js
+++ react/src/layouts/article/index.js
@@ -27,7 +27,7 @@ function Article() {
   const [snackbarSeverity, setSnackbarSeverity] = useState("success");
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   // Lấy danh sách bài viết từ API
   useEffect(() => {
@@ -169,7 +169,7 @@ function Article() {
                     rows={rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
                       console.log(row);
                       return {
-                        id : page * rowsPerPage + index + 1,
+                        id: page * rowsPerPage + index + 1,
                         fuction: (
                           <div className="container">
                             <div className="row">
