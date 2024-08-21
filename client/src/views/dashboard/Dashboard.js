a2a93ad0e7f6067fd26d698abc88655ecd16cd6a
Tranthikimdang <dangttkpc05468@fpt.edu.vn>
2024-08-21 23:53:53 +0700

    dashborad lấy dữ liệu api

--- client/src/views/dashboard/Dashboard.js
+++ client/src/views/dashboard/Dashboard.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageContainer from 'src/components/container/PageContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel } from 'react-bootstrap';
 import { Box, Button, Typography, Grid, Card, CardContent, CardMedia } from '@mui/material';
 import { styled } from '@mui/system';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+//api
+import categoriesApi from '../../apis/categoriesApi';
+import apis from '../../apis/articleApi';
+import userApi from '../../apis/userApi';
 
 const StyledBox = styled(Box)(({ theme }) => ({
   backgroundColor: '#8000ff',
@@ -50,81 +56,140 @@ const ImageBox = styled(Box)(({ theme }) => ({
   opacity: 0.9,
 }));
 
-// Sample data for the cards
-const featuredPosts = [
-  {
-    title: 'Tổng hợp các sản phẩm của học viên tại F8',
-    image: 'https://files.fullstack.edu.vn/f8-prod/blog_posts/65/6139fe28a9844.png',
-    author: 'Sơn Đặng',
-    timeToRead: '6 phút đọc',
-  },
-  {
-    title: '[Phần 1] Tạo dự án ReactJS với Webpack và Babel',
-    image: 'https://files.fullstack.edu.vn/f8-prod/blog_posts/279/6153f692d366e.jpg',
-    author: 'Sơn Đặng',
-    timeToRead: '12 phút đọc',
-  },
-  {
-    title: 'Cách đưa code lên GitHub và tạo GitHub Pages',
-    image: 'https://files.fullstack.edu.vn/f8-prod/blog_posts/677/615436b218d0a.png',
-    author: 'khadev27',
-    timeToRead: '4 phút đọc',
-  },
-  {
-    title: 'Ký sự ngày thứ 25 học ở F8',
-    image: 'https://files.fullstack.edu.vn/f8-prod/blog_posts/51/6139c6453456e.png',
-    author: 'Sơn Sơn',
-    timeToRead: '1 phút đọc',
-  },
-  {
-    title: 'Các nguồn tài nguyên hữu ích cho 1 front-end developer',
-    image: 'https://files.fullstack.edu.vn/f8-prod/blog_posts/107/613a1f3685814.png',
-    author: 'Dương Vương',
-    timeToRead: '2 phút đọc',
-  },
-  // Add more posts here
-];
-
 const Home = () => {
+  const [cates, setCates] = useState([]);
+  const [articles, setArticles] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await apis.getList();
+        if (response.status === 200) {
+          setArticles(response.data || []);
+          console.log("Fetched articles:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await userApi.getList();
+        if (response.status === 200) {
+          setUsers(response.data || []);
+          console.log("Fetched users:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await categoriesApi.getList();
+        if (response.status === 200) {
+          setCates(response.data || []);
+        }
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
   return (
     <PageContainer title="Dashboard" description="this is Dashboard">
       <Box>
+        {/* Banner */}
         <Grid >
-          <Carousel variant="dark">
+          <Carousel
+            variant="dark"
+            indicators={false}
+            interval={5000}
+            nextIcon={
+              <Box
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                right: '30px', // Adjust spacing from the right side
+                transform: 'translateY(-50%)',
+                width: '40px',
+                height: '40px',
+                backgroundColor: '#fff',
+                borderRadius: '50%',
+                boxShadow: '0 2px 5px rgba(0, 0, 0, 0.15)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                zIndex: 1,
+              }}
+            >
+                <ArrowForwardIcon sx={{ color: '#333', fontSize: '20px' }} />
+              </Box>
+            }
+            prevIcon={
+              <Box
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '30px', // Adjust spacing from the left side
+                transform: 'translateY(-50%)',
+                width: '40px',
+                height: '40px',
+                backgroundColor: '#fff',
+                borderRadius: '50%',
+                boxShadow: '0 2px 5px rgba(0, 0, 0, 0.15)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                zIndex: 1,
+              }}
+            >
+                <ArrowBackIcon sx={{ color: '#333', fontSize: '20px' }} />
+              </Box>
+            }
+          >
             {/* Carousel Item 1 */}
             <Carousel.Item>
-              <StyledBox sx={{ background: 'linear-gradient(90deg, #0072ff 0%, #00c6ff 100%)', borderRadius: '20px', padding: '20px' }}>
+              <StyledBox
+                sx={{
+                  background: 'linear-gradient(90deg, #f94f4f 0%, #ff8a00 100%)',
+                  borderRadius: '20px',
+                  padding: '20px',
+                }}
+              >
                 <Grid container spacing={2} alignItems="center">
-                  <Grid item xs={12} md={6}>
+                  <Grid item xs={12} md={8}>
                     <Typography variant="h4" component="h2" fontWeight="bold" sx={{ color: '#fff' }}>
-                      Học JavaScript Miễn Phí!
+                    Chào Mừng Đến Với Nền Tảng Chia Sẻ Kiến Thức!
                     </Typography>
                     <SubText sx={{ color: '#fff' }}>
-                      Khóa học JavaScript từ cơ bản tới nâng cao. Kết quả của khóa học này là bạn có thể làm hầu hết các dự án thường gặp với JavaScript.
+                    Nền tảng của chúng tôi là nơi các chuyên gia và người học có thể kết nối, chia sẻ kiến thức và kinh nghiệm trong nhiều lĩnh vực khác nhau. Khám phá các khóa học miễn phí, bài viết hữu ích, và tài liệu chất lượng để nâng cao kỹ năng và mở rộng hiểu biết của bạn.
                     </SubText>
-                    <ActionButton
-                      variant="outlined"
-                      sx={{
-                        color: '#fff',
-                        borderColor: '#fff',
-                        width: '100%',
-                        padding: '15px 0',
-                        marginTop: '15px',
-                        textAlign: 'center',
-                        fontWeight: 'bold',
-                        '&:hover': {
-                          backgroundColor: '#fff',
-                          color: '#0072ff',
-                          borderColor: '#fff',
-                        },
-                      }}
-                    >
-                      Đăng Ký Ngay
-                    </ActionButton>
                   </Grid>
-                  <Grid item xs={12} md={6} sx={{ textAlign: 'right' }}>
-                    <ImageBox sx={{ maxWidth: '80%', margin: '0 auto' }}>
-                      <img src="/path-to-your-image2.png" alt="JavaScript" style={{ width: '100%', borderRadius: '10px' }} />
+                  <Grid item xs={12} md={6} sx={{ textAlign: 'center' }}>
+                    <ImageBox sx={{ maxWidth: '90%', margin: '0 auto' }}>
+                      <img
+                        src="https://files.fullstack.edu.vn/f8-prod/banners/36/6454dee96205c.png"
+                        alt="JavaScript"
+                        style={{ width: '100%', borderRadius: '10px' }}
+                      />
                     </ImageBox>
                   </Grid>
                 </Grid>
@@ -132,7 +197,13 @@ const Home = () => {
             </Carousel.Item>
             {/* Carousel Item 2 */}
             <Carousel.Item>
-              <StyledBox sx={{ background: 'linear-gradient(90deg, #0072ff 0%, #00c6ff 100%)', borderRadius: '20px', padding: '20px' }}>
+              <StyledBox
+                sx={{
+                  background: 'linear-gradient(90deg, #f94f4f 0%, #ff8a00 100%)',
+                  borderRadius: '20px',
+                  padding: '20px',
+                }}
+              >
                 <Grid container spacing={2} alignItems="center">
                   <Grid item xs={12} md={6}>
                     <Typography variant="h4" component="h2" fontWeight="bold" sx={{ color: '#fff' }}>
@@ -146,14 +217,13 @@ const Home = () => {
                       sx={{
                         color: '#fff',
                         borderColor: '#fff',
-                        width: '100%',
-                        padding: '15px 0',
-                        marginTop: '15px',
-                        textAlign: 'center',
+                        width: 'auto',
+                        padding: '10px 20px',
+                        marginTop: '20px',
                         fontWeight: 'bold',
                         '&:hover': {
                           backgroundColor: '#fff',
-                          color: '#0072ff',
+                          color: '#f94f4f',
                           borderColor: '#fff',
                         },
                       }}
@@ -161,9 +231,13 @@ const Home = () => {
                       Đăng Ký Ngay
                     </ActionButton>
                   </Grid>
-                  <Grid item xs={12} md={6} sx={{ textAlign: 'right' }}>
-                    <ImageBox sx={{ maxWidth: '80%', margin: '0 auto' }}>
-                      <img src="/path-to-your-image2.png" alt="JavaScript" style={{ width: '100%', borderRadius: '10px' }} />
+                  <Grid item xs={12} md={6} sx={{ textAlign: 'center' }}>
+                    <ImageBox sx={{ maxWidth: '90%', margin: '0 auto' }}>
+                      <img
+                        src="https://files.fullstack.edu.vn/f8-prod/banners/Banner_04_2.png"
+                        alt="JavaScript"
+                        style={{ width: '100%', borderRadius: '10px' }}
+                      />
                     </ImageBox>
                   </Grid>
                 </Grid>
@@ -172,47 +246,45 @@ const Home = () => {
           </Carousel>
         </Grid>
 
-
-
         {/* Featured Posts Section */}
         <Grid container spacing={3} sx={{ marginBottom: { xs: '50px', md: '50px' }, marginTop: '30px' }}>
-          <Grid item xs={12}>
+          <Grid item xs={12} display="flex" justifyContent="space-between" alignItems="center">
             <Typography variant="h5" component="h2" fontWeight="bold">
               Bài viết nổi bật
             </Typography>
+            <Box component="a" href="/article" sx={{ textDecoration: 'none', color: '#5d86fe', fontWeight: 'bold' }}>
+              Xem tất cả &gt;
+            </Box>
           </Grid>
-          {featuredPosts.map((post, index) => (
-            <Grid item xs={12} sm={6} md={3} key={index}>
-              <Card>
+          {articles.map((article) => (
+            <Grid item xs={12} sm={6} md={3} key={article?.id}>
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <CardMedia
                   component="img"
                   height="140"
-                  image={post.image}
-                  alt={post.title}
+                  image={article.image}
+                  alt={article.title}
                 />
-                <CardContent>
-                  <Typography variant="h6" component="h2">
-                    {post.title}
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
+                  <Typography variant="h6" component="h2" sx={{ mb: 2 }}>
+                    {article.title}
                   </Typography>
-                  <Typography variant="body2" color="textSecondary">
-                    {post.author} • {post.timeToRead}
-                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 'auto' }}>
+                    <Typography variant="body2" color="textSecondary">
+                      {users?.find(u => article?.user_id === u.id)?.name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {article.updated_at}
+                    </Typography>
+                  </Box>
                 </CardContent>
               </Card>
             </Grid>
           ))}
         </Grid>
-
       </Box>
     </PageContainer>
   );
 };
 
 export default Home;
-
-
-
-
-
-
-
