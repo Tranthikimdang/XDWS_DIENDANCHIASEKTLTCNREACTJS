1b290b84c7a452b51284ada2bae7400077e6690a
Linhle1284 <linhlnpc06539@fpt.edu.vn>
2024-10-03 08:45:58 +0700

    fix

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -9,64 +9,37 @@ import ResetPassword from '../views/authentication/ResetPassword';
 const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
 const BlankLayout = Loadable(lazy(() => import('../layouts/blank/BlankLayout')));
 const AdminLayout = Loadable(lazy(() => import('../layouts/admin')));
+
 /* ****Pages***** */
-const Dashboard = Loadable(lazy(() => import('../views/dashboard/Dashboard')))
-const Article = Loadable(lazy(() => import('../views/article/Article')))
-const ArticleDetail = Loadable(lazy(() => import('../views/article/components/ArticleDetail'))) 
-const Newpost = Loadable(lazy(() => import('../views/article/components/new-post.js'))) 
-const SamplePage = Loadable(lazy(() => import('../views/sample-page/SamplePage')))
-const Icons = Loadable(lazy(() => import('../views/icons/Icons')))
-const TypographyPage = Loadable(lazy(() => import('../views/utilities/TypographyPage')))
-const Shadow = Loadable(lazy(() => import('../views/utilities/Shadow')))
 const Dashboard = Loadable(lazy(() => import('../views/dashboard/Dashboard')));
 const Article = Loadable(lazy(() => import('../views/article/Article')));
-const ArticleDetail = Loadable(lazy(() => import('../views/article/components/ArticleDetail'))); // Import the ArticleDetail component
-const Newpost = Loadable(lazy(() => import('../views/article/components/new-post.js'))); // Import the ArticleDetail component
+const ArticleDetail = Loadable(lazy(() => import('../views/article/components/ArticleDetail')));
+const Newpost = Loadable(lazy(() => import('../views/article/components/new-post.js')));
 const SamplePage = Loadable(lazy(() => import('../views/sample-page/SamplePage')));
 const Icons = Loadable(lazy(() => import('../views/icons/Icons')));
 const TypographyPage = Loadable(lazy(() => import('../views/utilities/TypographyPage')));
 const Shadow = Loadable(lazy(() => import('../views/utilities/Shadow')));
 const ProductClient = Loadable(lazy(() => import('../views/product/index')));
-
 const Error = Loadable(lazy(() => import('../views/authentication/Error')));
 const Register = Loadable(lazy(() => import('../views/authentication/Register')));
 const Login = Loadable(lazy(() => import('../views/authentication/Login')));
-const UserList = Loadable(lazy(() => import('../views/User/User'))); // Import the UserList component
+const UserList = Loadable(lazy(() => import('../views/User/User')));
 
-const DashboardAdmin = Loadable(lazy(() => import('../pages/admin/dashboard')))
-
-const ArticleAdmin = Loadable(lazy(() => import('../pages/admin/article')))
-const FormAddArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormAddArticle')))
-const FormEditArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormEditArticle')))
-const FormViewArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormViewArticle')))
-
-const CommentDetailAdmin = Loadable(lazy(() => import('../pages/admin/commentDetail')))
-const CategoryAdmin = Loadable(lazy(() => import('../pages/admin/category')))
-const ProfileAdmin = Loadable(lazy(() => import('../pages/admin/profile')))
-const CommentAdmin = Loadable(lazy(() => import('../pages/admin/comment')))
-const QuestionAdmin = Loadable(lazy(() => import('../pages/admin/questions')))
-const UserAdmin = Loadable(lazy(() => import('../pages/admin/user')))
-
-const CategoryPro = Loadable(lazy(() => import("../pages/admin/category_pro")))
-const AddCatePro = Loadable(lazy(() => import("../pages/admin/category_pro/data/FormAddCate")))
-const EditCatePro = Loadable(lazy(() => import("../pages/admin/category_pro/data/FormEditCate")))
-///////danh muc bai viet
-const Category = Loadable(lazy(() => import("../pages/admin/category")))
-const AddCate = Loadable(lazy(() => import("../pages/admin/category/data/FormAddCate")))
-const EditCate = Loadable(lazy(() => import("../pages/admin/category/data/FormEditCate")))
+/* ****Admin Pages***** */
 const DashboardAdmin = Loadable(lazy(() => import('../pages/admin/dashboard')));
 const ArticleAdmin = Loadable(lazy(() => import('../pages/admin/article')));
+const FormAddArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormAddArticle')));
+const FormEditArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormEditArticle')));
+const FormViewArticle = Loadable(lazy(() => import('../pages/admin/article/data/FormViewArticle')));
 const CommentDetailAdmin = Loadable(lazy(() => import('../pages/admin/commentDetail')));
 const CategoryAdmin = Loadable(lazy(() => import('../pages/admin/category')));
 const ProfileAdmin = Loadable(lazy(() => import('../pages/admin/profile')));
 const CommentAdmin = Loadable(lazy(() => import('../pages/admin/comment')));
 const QuestionAdmin = Loadable(lazy(() => import('../pages/admin/questions')));
 const UserAdmin = Loadable(lazy(() => import('../pages/admin/user')));
-
 const CategoryPro = Loadable(lazy(() => import('../pages/admin/category_pro')));
 const AddCatePro = Loadable(lazy(() => import('../pages/admin/category_pro/data/FormAddCate')));
 const EditCatePro = Loadable(lazy(() => import('../pages/admin/category_pro/data/FormEditCate')));
-
 const Product = Loadable(lazy(() => import('../pages/admin/product')));
 const AddProduct = Loadable(lazy(() => import('../pages/admin/product/data/FormAddProduct')));
 const EditProduct = Loadable(lazy(() => import('../pages/admin/product/data/FormEditProduct')));
@@ -82,105 +55,65 @@ const renderRoutes = (role) => {
         { path: 'login', element: <Login /> },
         { path: '/auth/forgot-password', element: <ForgotPassword /> },
         { path: '/auth/reset-password/:userId', element: <ResetPassword /> },
-        // { path: '*', element: <Navigate to="/auth/404" /> },
       ],
     },
     { path: '*', element: <>TRang không tồn tại</> },
   ];
-  if (!role) return routes;
+
+  const userRoutes = [
+    { path: '/', element: <Navigate to="/home" /> },
+    { path: '/home', exact: true, element: <Dashboard /> },
+    { path: '/article', exact: true, element: <Article /> },
+    { path: '/article/:id', exact: true, element: <ArticleDetail /> },
+    { path: '/new-post', exact: true, element: <Newpost /> },
+    { path: '/sample-page', exact: true, element: <SamplePage /> },
+    { path: '/icons', exact: true, element: <Icons /> },
+    { path: '/ui/typography', exact: true, element: <TypographyPage /> },
+    { path: '/ui/shadow', exact: true, element: <Shadow /> },
+    { path: '/user', exact: true, element: <UserList /> },
+    { path: '/profile', exact: true, element: <Profile /> },
+    { path: '/products', exact: true, element: <ProductClient /> },
+  ];
+
+  const adminRoutes = [
+    { path: 'dashboard', element: <DashboardAdmin /> },
+    { path: 'article', exact: true, element: <ArticleAdmin /> },
+    { path: 'formaddarticle', exact: true, element: <FormAddArticle /> },
+    { path: 'formeditarticle', exact: true, element: <FormEditArticle /> },
+    { path: 'formviewarticle/:id', exact: true, element: <FormViewArticle /> },
+    { path: 'comment', exact: true, element: <CommentAdmin /> },
+    { path: 'category', exact: true, element: <CategoryAdmin /> },
+    { path: 'profile', exact: true, element: <ProfileAdmin /> },
+    { path: 'questions', exact: true, element: <QuestionAdmin /> },
+    { path: 'user', exact: true, element: <UserAdmin /> },
+    { path: 'comment-detail', exact: true, element: <CommentDetailAdmin /> },
+    { path: 'categoryPro', exact: true, element: <CategoryPro /> },
+    { path: 'addCatePro', exact: true, element: <AddCatePro /> },
+    { path: 'editCatePro/:id', exact: true, element: <EditCatePro /> },
+    { path: 'products', exact: true, element: <Product /> },
+    { path: 'editProduct/:id', exact: true, element: <EditProduct /> },
+    { path: 'addProduct', exact: true, element: <AddProduct /> },
+  ];
+
   if (role === 'admin') {
     routes.push({
       path: '/',
       element: <FullLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/home" /> },
-        { path: '/home', exact: true, element: <Dashboard /> },
-        { path: '/article', exact: true, element: <Article /> },
-        { path: '/article/:id', exact: true, element: <ArticleDetail /> }, // Add this route
-        { path: '/new-post', exact: true, element: <Newpost /> }, // Add this route
-        { path: '/sample-page', exact: true, element: <SamplePage /> },
-        { path: '/icons', exact: true, element: <Icons /> },
-        { path: '/ui/typography', exact: true, element: <TypographyPage /> },
-        { path: '/ui/shadow', exact: true, element: <Shadow /> },
-        { path: '/user', exact: true, element: <UserList /> }, // Add this route for UserList
-        { path: '/profile', exact: true, element: <Profile /> },
-        { path: '/products', exact: true, element: <ProductClient /> },
-
-        // { path: '*', element: <Navigate to="/auth/404" /> },
-      ],
+      children: userRoutes,
     });
     routes.push({
       path: '/admin',
       element: <AdminLayout />,
-      children: [
-        { path: 'dashboard', element: <DashboardAdmin/> },
-        { path: 'article', exact: true, element: <ArticleAdmin/> },
-        { path: 'formaddarticle', exact: true, element: <FormAddArticle/> },
-        { path: 'formeditarticle', exact: true, element: <FormEditArticle/> },
-        { path: 'formviewarticle/:id', exact: true, element: <FormViewArticle/> },
-        { path: 'dashboard', element: <DashboardAdmin /> },
-        { path: 'article', exact: true, element: <ArticleAdmin /> },
-        { path: 'comment', exact: true, element: <CommentAdmin /> }, // Add this route
-        { path: 'category', exact: true, element: <CategoryAdmin /> },
-        { path: 'profile', exact: true, element: <ProfileAdmin /> },
-        { path: 'questions', exact: true, element: <QuestionAdmin /> },
-        { path: 'user', exact: true, element: <UserAdmin /> },
-        { path: 'comment-detail', exact: true, element: <CommentDetailAdmin /> }, // Add this route for UserList
-        { path: 'categoryPro', exact: true, element: <CategoryPro /> },
-        { path: 'addCatePro', exact: true, element: <AddCatePro /> },
-        { path: 'editCatePro/:id', exact: true, element: <EditCatePro /> },
-        { path: 'category', exact: true, element: <Category /> },
-        { path: 'addCate', exact: true, element: <AddCate /> },
-        { path: 'editCate/:id', exact: true, element: <EditCate /> },
-        { path: 'products', exact: true, element: <Product /> },
-        { path: 'editProduct/:id', exact: true, element: <EditProduct /> },
-        { path: 'addProduct', exact: true, element: <AddProduct /> },
-        // { path: '*', element: <Navigate to="/auth/404" /> },
-      ],
+      children: adminRoutes,
     });
   } else if (role === 'user') {
     routes.push({
       path: '/',
       element: <FullLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/home" /> },
-        { path: '/home', exact: true, element: <Dashboard /> },
-        { path: '/article', exact: true, element: <Article /> },
-        { path: '/article/:id', exact: true, element: <ArticleDetail /> }, // Add this route
-        { path: '/new-post', exact: true, element: <Newpost /> }, // Add this route
-        { path: '/sample-page', exact: true, element: <SamplePage /> },
-        { path: '/icons', exact: true, element: <Icons /> },
-        { path: '/ui/typography', exact: true, element: <TypographyPage /> },
-        { path: '/ui/shadow', exact: true, element: <Shadow /> },
-        { path: '/user', exact: true, element: <UserList /> }, // Add this route for UserList
-        { path: '/profile', exact: true, element: <Profile /> },
-        { path: '/products', exact: true, element: <ProductClient /> },
-
-        // { path: '*', element: <Navigate to="/auth/404" /> },
-      ],
-    });
-  } else {
-    routes.push({
-      path: '/',
-      element: <FullLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/home" /> },
-        { path: '/home', exact: true, element: <Dashboard /> },
-        { path: '/article', exact: true, element: <Article /> },
-        { path: '/article/:id', exact: true, element: <ArticleDetail /> }, // Add this route
-        { path: '/new-post', exact: true, element: <Newpost /> }, // Add this route
-        { path: '/sample-page', exact: true, element: <SamplePage /> },
-        { path: '/icons', exact: true, element: <Icons /> },
-        { path: '/ui/typography', exact: true, element: <TypographyPage /> },
-        { path: '/ui/shadow', exact: true, element: <Shadow /> },
-        { path: '/user', exact: true, element: <UserList /> }, // Add this route for UserList
-        { path: '/profile', exact: true, element: <Profile /> },
-        { path: '/products', exact: true, element: <ProductClient /> },
-
-        // { path: '*', element: <Navigate to="/auth/404" /> },
-      ],
+      children: userRoutes,
     });
   }
+
   return routes;
 };
 
